8582b14e44b171937dde0a82e0a9bdb5b9e7a33e
pimms <joakim.stien@gmail.com>
2013-09-06 20:44:27 +0200

    Added fancy shit

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -5,11 +5,32 @@ using System.Collections;
  * fashion.
  */
 public class CameraMovement : MonoBehaviour {
+	public float movementFactor = 0.1f;
+
+	private float lastMouseX;
+	private float lastMouseY;
+
 	void Start () {
-	
+		
 	}
 	
 	void Update () {
-	
+		if (Input.GetMouseButton(2)) {
+			float mouseX = Input.mousePosition.x;
+			float mouseY = Input.mousePosition.y;
+
+			Vector3 euler = transform.rotation.ToEuler();
+
+			float deltaX = mouseX - lastMouseX;
+			float deltaY = mouseY - lastMouseY;
+
+			// TODO: SOME FANCY SHIT
+			
+			lastMouseX = mouseX;
+			lastMouseY = mouseY;
+		} else {
+			lastMouseX = Input.mousePosition.x;
+			lastMouseY = Input.mousePosition.y;
+		}
 	}
 }

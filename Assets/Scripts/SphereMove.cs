da39ec80b44975de460c8f6efe5b076b1c21f40f
pimms <joakim.stien@gmail.com>
2013-09-06 19:15:04 +0200

    added folders for stuff, and some basic stuff

---
+++ Assets/Scripts/SphereMove.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereMove : MonoBehaviour {
+
+	public GameObject plane;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = plane.transform.position;
+		pos.x += 0.1f;
+		plane.transform.position = pos;
+	}
+}

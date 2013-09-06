c0bf6fea875424f675db8090011660bd6250514d
pimms <joakim.stien@gmail.com>
2013-09-06 20:05:17 +0200

    Added base script for camera movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* Class used for moving the camera in an RTS-like
+ * fashion.
+ */
+public class CameraMovement : MonoBehaviour {
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}

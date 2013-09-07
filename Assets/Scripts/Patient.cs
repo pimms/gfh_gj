fbb9009c82acd7d1f56643788f3de8efc59c790b
pimms <joakim.stien@gmail.com>
2013-09-07 18:57:10 +0200

    Added patient values

--- Assets/Scripts/Patient.cs
+++ Assets/Scripts/Patient.cs
@@ -5,14 +5,41 @@ public class Patient : Person {
 	// Levels of sickness: 1 is difficult, 5 is easy.
 	public double sickness = 1;
 
-	public static int patBedRates[,];		
-	public static int patOutRates[,];
-	public static int patSurgRates[,];
-	public static int patHealthSec[,];
+	public static int[,] patBedRates = {
+		{5, 90, 5},
+		{25, 65, 10},
+		{20, 60, 20},
+		{15, 55, 30},
+		{10, 0, 90},
+	};
+	public static int[,] patOutRates = {
+		{30, 70, 0},
+		{60, 30, 10},
+		{50, 45, 5},
+		{40, 45, 15},
+		{30, 50, 20},
+	};
+	public static int[,] patSurgRates = {
+		{50, 50},
+		{40, 60},
+		{30, 70},
+		{20, 80},
+		{10, 90},			
+	};
+	public static int[,] patHealthSec = {
+		{-10, 2, -40},
+		{20, -20, -25},
+		{10, -10, -20},
+		{10, -5, -15},
+		{20, -5, -10},	
+	};
+
 
 	protected override void Start() {
 		base.Start();
 		walkSpeed = 6f;
+
+		
 	}
 
 	void Update() {

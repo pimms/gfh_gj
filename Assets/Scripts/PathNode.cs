1a7a67773485004734adb47da27c6dafc0764649
pimms <joakim.stien@gmail.com>
2013-09-07 10:00:15 +0200

    Added pathfinding algorithm

--- Assets/Scripts/PathNode.cs
+++ Assets/Scripts/PathNode.cs
@@ -5,11 +5,38 @@ using System.Collections.Generic;
 public class PathNode : MonoBehaviour {
 	public List<PathNode> neighbours;
 
+	private int g;
+	private int h;
+	private PathNode parent;
+
+	public int G {
+		set { g = value; }
+		get { return g; }
+	}
+
+	public int H {
+		set { h = value; }
+		get { return h; }
+	}
+
+	public int F {
+		get { return g + h; }
+	}
+
+	public PathNode Parent {
+		set { parent = value; }
+		get { return parent; }
+	}
+
 	void Start () {
 	
 	}
 	
 	void Update () {
 	
+	}
+
+	public void Reset() {
+		
 	}
 }

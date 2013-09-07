d2927ead8dc71a0ba8d846f36c228fbdfc73285e
pimms <joakim.stien@gmail.com>
2013-09-07 21:58:09 +0200

    Added patient manager

---
+++ Assets/Scripts/PatientManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PatientManager : MonoBehaviour {
+	public GameObject pfPatient;
+
+	private float timer = 10f;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		
+	}
+
+	private void SpawnPatient() {
+		
+	}
+}

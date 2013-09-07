d61a80ea54e25ec70f5471b27218e08e2599a4c9
Kneltix <janhenrikth@gmail.com>
2013-09-07 13:28:10 +0200

    ????

---
+++ Assets/Scripts/Money.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Money : MonoBehaviour {
+
+	int money = 100;
+	
+	int AddMoney(){
+		if (Patient.Operation == true){
+			Money += Patient.Money();
+		}
+	}
+	
+	
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/MoneyText.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MoneyText : MonoBehaviour {
+
+	
+	int ShowMoney(){
+		guiText.text = "Money = ";
+		
+	}
+	
+	// Use this for initialization
+	void Start () {
+		ShowMoney ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

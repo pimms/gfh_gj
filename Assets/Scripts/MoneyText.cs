27041138dcd66e9a71128a50b4ff8c13b7909105
Kneltix <janhenrikth@gmail.com>
2013-09-07 19:04:04 +0200

    AAAAANDDDDDYYYYY

--- Assets/Scripts/MoneyText.cs
+++ Assets/Scripts/MoneyText.cs
@@ -17,7 +17,7 @@ public class MoneyText : MonoBehaviour {
 	
 	void OnGUI(){
 		
-		GUI.Box(new Rect(10,10,50,25), MoneyInAccount+ " £");
+		GUI.Box(new Rect(10,10,50,25),"$" +MoneyInAccount);
 
 	}
 }

b2e6c37e2396c9e44531f61e4801550b0e0ff4c1
Kneltix <janhenrikth@gmail.com>
2013-09-07 16:35:37 +0200

    Money

--- Assets/Scripts/Money.cs
+++ Assets/Scripts/Money.cs
@@ -1,25 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-
 public class Money : MonoBehaviour {
-<<<<<<< HEAD
 
-		// Use this for initialization
-=======
-	/*
-	int money = 100;
-	
-	int AddMoney(){
-		if (Patient.Operation == true){
-			Money += Patient.Money();
-		}
-	}
-	
-	
-	
 	// Use this for initialization
->>>>>>> 8779e9d40b0889dc71d34c389fa45a33f544c431
 	void Start () {
 	
 	}
@@ -28,19 +12,4 @@ public class Money : MonoBehaviour {
 	void Update () {
 	
 	}
-<<<<<<< HEAD
-	
-	float MoneyInAccount = 100;
-
-
-	void AddMoneyPatient(){
-		if (Patient.Operation == true){
-			MoneyInAccount += Patient.Money();
-		}else{
-			//FUCK OFF!
-		}
-	}
-=======
-	 */
->>>>>>> 8779e9d40b0889dc71d34c389fa45a33f544c431
 }
--- Assets/Scripts/MoneyText.cs
+++ Assets/Scripts/MoneyText.cs
@@ -1,23 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-
 public class MoneyText : MonoBehaviour {
-	/*
-	
+
 	// Use this for initialization
 	void Start () {
-
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		print(Money.MoneyInAccount);
 	
 	}
-<<<<<<< HEAD
 	
-=======
-	 * */
->>>>>>> 8779e9d40b0889dc71d34c389fa45a33f544c431
+	GUI.Box (Rect(10,10,50,25),  );
 }

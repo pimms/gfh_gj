a56b18347af7d912cfcb57ecbdc95cb8a237f850
Azervu <sndr.hell@gmail.com>
2013-09-08 11:44:31 +0200

    NurseEnergy

--- Assets/Scripts/Nurse.cs
+++ Assets/Scripts/Nurse.cs
@@ -5,21 +5,71 @@ using System.Collections;
 public class Nurse : Person {
 	const double MAXEXP = 100;
 	public double exp = 10;
+	
+	protected float energyTimer;
+	
+	void OnGUI() {
 
+		Rect rect = new Rect(0f, 0f, Screen.width, Screen.height);
+		Vector2 pos = Camera.main.WorldToScreenPoint(transform.position);
+
+		if (rect.Contains(pos)) {
+			float distance = (Camera.main.transform.position - transform.position).magnitude;
+			if (distance < 35f) {
+				GUI.color = Color.white;
+				GUI.backgroundColor = new Color(1f, 1f, 1f, 1f);
+
+				Rect box = new Rect(pos.x - 400 / distance, Screen.height - pos.y - 1000 / distance, health / 2f, 10f);
+				GUI.Button(box, "");
+			}
+		}
+	}
+	
+	
 	protected override void Start() {
 		base.Start();
-
 		walkSpeed = 8f;
 	}
 
-	void Update() {
+	protected override void Update() {
 		base.Update();
+		energyUpdate();
 
 		Vector3 pos = transform.position;
 		pos.y = 1f;
 		transform.position = pos;
 	}
+	
+	public float efficieny(){
+		return (0.75f+health/200f);
+		
+	}
+	
+	public void energyUpdate() {
+		
+		float delta = Time.deltaTime;
+		
+		Sofa sofaPtr = currentBed as Sofa;
+		
+		if ( sofaPtr != null) {
+				if (health < 100f) {
+				health += (100f/20f) * Time.deltaTime;
+			}
+			
+		} else if (currentBed != null) {
+			if (health > 0f) {
+				health -= (100f/180f) * Time.deltaTime;
+			}
+			
+		} else {
+			if (health < 100f) {
+				health += (100f/90f) * Time.deltaTime;
+			}
+		}
 
+	}
+	
+	
 	public override void OnMouseClick(int mouseButton, InputOrder inOrder) {
 		inOrder.AddAsActor(this);
 	}
